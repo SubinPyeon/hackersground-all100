037581eae60eda06921db0f1d6325542c1792c19
Subin Pyeon <105070397+SubinPyeon@users.noreply.github.com>
2023-06-22 19:16:53 +0000

    update new 1

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,16 +1,17 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// 웹 서버에서 정적 파일을 제공하기 위한 설정
-builder.WebHost.UseWebRoot("wwwroot");
-
 var app = builder.Build();
 
-// 정적 파일 제공을 위한 미들웨어 등록
-app.UseStaticFiles();
+app.UseRouting();
+
+app.MapGet("/", async context =>
+{
+    var message = "Hello, World!";
+    await context.Response.WriteAsync($"<h1>{message}</h1>");
+});
 
 if (app.Environment.IsDevelopment())
 {
